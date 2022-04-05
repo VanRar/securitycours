1a0e63eafde7e00f70509f87db128c1562150d02
VanRar <vitalikf7@gmail.com>
2022-04-05 21:09:30 +0300

    Start project, added rest and model

---
+++ src/main/java/ru/vanrar/securitycours/SecuritycoursApplication.java
@@ -0,0 +1,13 @@
+package ru.vanrar.securitycours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritycoursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritycoursApplication.class, args);
+    }
+
+}
